cb9872ce542ba40abe2ff932341c27b1cc170355
gigin <giginmaryabraham@gmail.com>
2023-05-22 08:47:00 +0530

    chnages

--- Tex/src/main/java/ExcelCode.java
+++ Tex/src/main/java/ExcelCode.java
@@ -19,7 +19,7 @@ public class ExcelCode {
 	public static String getStringData(int a, int b) throws IOException {
 
 		f = new FileInputStream("\\D:\\testexcel.xlsx");//excel file path
-		w = new XSSFWorkbook(f);
+		w = new XSSFWorkbook(f);//file
 		s = w.getSheet("Sheet1");
 		Row r = s.getRow(a);
 		Cell c = r.getCell(b);
